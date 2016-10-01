a3cef7ea75151b85f419ff5e863c3a9cb8581460
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2016-10-01 22:40:54 +0530

    Adding first cut of SuuchiServer which by default has PingService configured

---
+++ src/main/scala/in/ashwanthkumar/suuchi/rpc/SuuchiServer.scala
@@ -0,0 +1,46 @@
+package in.ashwanthkumar.suuchi.rpc
+
+import io.grpc.{BindableService, ServerBuilder, Server}
+import io.grpc.netty.NettyServerBuilder
+import org.slf4j.LoggerFactory
+
+class SuuchiServer(port: Int, services: List[BindableService] = Nil) {
+  private val log = LoggerFactory.getLogger(classOf[SuuchiServer])
+
+  private var server: Server = _
+  private val serverBuilder: ServerBuilder[_] = NettyServerBuilder.forPort(port).addService(new PingService)
+
+  def start() = {
+    services.foreach(serverBuilder.addService)
+    server = serverBuilder
+      .build()
+      .start()
+    log.info("Server started, listening on " + port)
+    Runtime.getRuntime.addShutdownHook(new Thread() {
+      override def run() {
+        // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+        System.err.println("*** shutting down gRPC server since JVM is shutting down")
+        SuuchiServer.this.stop()
+        System.err.println("*** server shut down")
+      }
+    })
+  }
+
+  def stop() = {
+    if (server != null) {
+      server.shutdown()
+    }
+  }
+
+  def blockUntilShutdown() = {
+    if (server != null) {
+      server.awaitTermination()
+    }
+  }
+}
+
+object SuuchiServer extends App {
+  val server = new SuuchiServer(5051)
+  server.start()
+  server.blockUntilShutdown()
+}
