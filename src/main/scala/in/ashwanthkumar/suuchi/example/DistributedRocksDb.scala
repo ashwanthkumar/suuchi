5dae18ef3bd60a54dfa4c352d30e8bed771fdb82
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2016-10-05 14:58:47 +0530

    Updating DistributedRocksDb.scala with replication changes from master

--- src/main/scala/in/ashwanthkumar/suuchi/example/DistributedRocksDb.scala
+++ src/main/scala/in/ashwanthkumar/suuchi/example/DistributedRocksDb.scala
@@ -9,22 +9,20 @@ import in.ashwanthkumar.suuchi.store.rocksdb.{RocksDbConfiguration, RocksDbStore
 import io.grpc.netty.NettyServerBuilder
 
 object DistributedRocksDb extends App {
-  val path1 = Files.createTempDirectory("distributed-rocksdb").toFile
-  val path2 = Files.createTempDirectory("distributed-rocksdb").toFile
-
-  val store1 = new RocksDbStore(RocksDbConfiguration(path1.getAbsolutePath))
-  val store2 = new RocksDbStore(RocksDbConfiguration(path2.getAbsolutePath))
-
   val routingStrategy = ConsistentHashingRouting(2, whoami(5051), whoami(5052))
 
+  val path1 = Files.createTempDirectory("distributed-rocksdb").toFile
+  val store1 = new RocksDbStore(RocksDbConfiguration(path1.getAbsolutePath))
   val server1 = Server(NettyServerBuilder.forPort(5051), whoami(5051))
     .routeUsing(new SuuchiReadService(store1), routingStrategy)
-    .routeUsing(new SuuchiPutService(store1), routingStrategy)
+    .withReplication(new SuuchiPutService(store1), 2, routingStrategy)
   server1.start()
 
+  val path2 = Files.createTempDirectory("distributed-rocksdb").toFile
+  val store2 = new RocksDbStore(RocksDbConfiguration(path2.getAbsolutePath))
   val server2 = Server(NettyServerBuilder.forPort(5052), whoami(5052))
     .routeUsing(new SuuchiReadService(store2), routingStrategy)
-    .routeUsing(new SuuchiPutService(store2), routingStrategy)
+    .withReplication(new SuuchiPutService(store2), 2, routingStrategy)
   server2.start()
 
   server1.blockUntilShutdown()
