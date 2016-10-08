dc638e1f83927a80276e7534658a50d2e9d8de52
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2016-10-09 03:34:36 +0530

    Adding some scaladocs to Bootstrapper

--- suuchi-core/src/main/scala/in/ashwanthkumar/suuchi/membership/Bootstrapper.scala
+++ suuchi-core/src/main/scala/in/ashwanthkumar/suuchi/membership/Bootstrapper.scala
@@ -2,10 +2,20 @@ package in.ashwanthkumar.suuchi.membership
 
 trait Bootstrapper {
   /**
-   * Returns a list of host
-   * @return
+   * Returns a list of host that're used to connect to form a cluster. These nodes always represent the
+   * initial seed nodes. While different [[Membership]] implementations might have different guarantees,
+   * It's generally a good practice to expose a single consistent copy of the nodes
+   * as seed nodes.
+   *
+   * @return  List[MemberAddress] that represents seed nodes to connect to
    */
   def nodes: List[MemberAddress]
 }
 
+/**
+ * Default in memory implementation of [[Bootstrapper]] to be used in tests and static
+ * configuration file based environments.
+ *
+ * @param nodes List[MemberAddress] that represents seed nodes to connect to
+ */
 case class InMemoryBootstrapper(override val nodes: List[MemberAddress]) extends Bootstrapper
