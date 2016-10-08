2340c790c232076231dc469b66b3d6df3263b505
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2016-10-08 09:22:46 +0530

    Using a countdownLatch for sync and max wait timeout of 60 seconds

--- suuchi-membership-scalecube/src/test/scala/in/ashwanthkumar/suuchi/membership/scalecube/ScaleCubeMembershipIT.scala
+++ suuchi-membership-scalecube/src/test/scala/in/ashwanthkumar/suuchi/membership/scalecube/ScaleCubeMembershipIT.scala
@@ -1,25 +1,34 @@
 package in.ashwanthkumar.suuchi.membership.scalecube
 
+import java.util.concurrent.{CountDownLatch, TimeUnit}
+
 import in.ashwanthkumar.suuchi.membership.{InMemoryBootstrapper, MemberAddress, Membership}
 import io.scalecube.cluster.gossip.GossipConfig
-import org.scalatest.Matchers.{convertToAnyShouldWrapper, have}
+import org.scalatest.Matchers.{be, convertToAnyShouldWrapper, have}
 import org.scalatest.{BeforeAndAfter, FlatSpec}
+
+class TestScaleCubeMembership(latch: CountDownLatch, port: Int, gossipConfig: Option[GossipConfig]) extends ScaleCubeMembership(port, gossipConfig) {
+  override def onJoin: (MemberAddress) => Unit = (m: MemberAddress) => {
+    super.onJoin(m)
+    println("Counting down")
+    latch.countDown()
+  }
+}
 
 class ScaleCubeMembershipIT extends FlatSpec with BeforeAndAfter {
   val BASE_PORT = 20000
   var members: List[Membership] = List()
+  val latch = new CountDownLatch(4) // at least 4 members should have joined
 
   before {
     val bootstrapper = InMemoryBootstrapper(List(MemberAddress("localhost", BASE_PORT + 1)))
-    (1 to 5).foreach { i =>
-      val memberPort = BASE_PORT + i
-      val gossipConfig = GossipConfig.builder().gossipInterval(5).build()
-      val member = new ScaleCubeMembership(memberPort, Some(gossipConfig))
-      if (i > 1) {
-        members = members ++ List(member.bootstrap(bootstrapper))
-      } else {
-        members = members ++ List(member.bootstrap(InMemoryBootstrapper(List())))
-      }
+    val gossipConfig = GossipConfig.builder().gossipInterval(5).build()
+    val seedNode = new TestScaleCubeMembership(latch, BASE_PORT + 1, Some(gossipConfig))
+    members = List(seedNode.bootstrap(InMemoryBootstrapper(List())))
+
+    (2 to 5).foreach { i =>
+      val member = new ScaleCubeMembership(BASE_PORT + i, Some(gossipConfig))
+      members = members ++ List(member.bootstrap(bootstrapper))
     }
   }
 
@@ -29,7 +38,8 @@ class ScaleCubeMembershipIT extends FlatSpec with BeforeAndAfter {
 
   "ScaleCubeCluster" should "launch 5 nodes and say they have 5 nodes" in {
     members.foreach(_.start())
-    Thread.sleep(10 * 1000) // wait for all the nodes to come up and gossip
+    val status = latch.await(60, TimeUnit.SECONDS) // wait until all nodes have contacted with the seed node
+    status should be(true)
 
     members.map(m => m.nodes).foreach(println)
     val totalNodes = members.head.nodes
