f830cd97354cbc9ee9c27a41e6aad9018de10258
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2016-10-11 23:42:30 +0530

    Moving logOnError to Logging for re-use

--- suuchi-core/src/main/scala/in/ashwanthkumar/suuchi/utils/Logging.scala
+++ suuchi-core/src/main/scala/in/ashwanthkumar/suuchi/utils/Logging.scala
@@ -2,6 +2,19 @@ package in.ashwanthkumar.suuchi.utils
 
 import org.slf4j.LoggerFactory
 
+import scala.util.Try
+
 trait Logging { self =>
   val log = LoggerFactory.getLogger(this.getClass)
+
+
+  def logOnError[T](f: () => T): Try[T] = {
+    Try {
+      f()
+    } recover {
+      case e: Exception =>
+        log.error(e.getMessage, e)
+        throw e
+    }
+  }
 }
