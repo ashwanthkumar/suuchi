f5bedfaac62b71f5ca8e77639b739cd87a0c583d
Selvaram Ganesh <gsriram7@gmail.com>
2016-11-29 19:54:19 +0530

    Introducing the notion of shards as an encapsulation of primary and secondary partitions

---
+++ suuchi-core/src/main/scala/in/ashwanthkumar/suuchi/partitioner/CHRWithShards.scala
@@ -0,0 +1,53 @@
+package in.ashwanthkumar.suuchi.partitioner
+
+import java.util
+
+import in.ashwanthkumar.suuchi.cluster.MemberAddress
+
+case class Shard(primaryPartition: VNode, follower: List[VNode]) {
+  def key = primaryPartition.node.host + "_" + primaryPartition.node.port + "_" + primaryPartition.nodeReplicaId
+  def getNodes = primaryPartition.node :: follower.map(_.node)
+}
+
+class CHRWithShards(hashFn: Hash, partitionsPerNode: Int, replicationFactor: Int = 2) {
+  val sortedMap = new util.TreeMap[Integer, Shard]()
+
+  val MAX_DUPES = 10
+
+  def init(nodes: List[MemberAddress]) = {
+    nodes.foreach(node => add(node, nodes))
+    this
+  }
+
+  private def hash(shard: Shard): Int = hashFn.hash(shard.key.getBytes)
+
+  def followers(nodes: List[MemberAddress]): List[VNode] = {
+    nodes.flatMap(node => (1 until replicationFactor).map(i => VNode(node, i, Follower)))
+  }
+
+  def add(node: MemberAddress, nodes: List[MemberAddress]) = {
+    (1 to partitionsPerNode).map(i => Shard(VNode(node, 1, Primary), followers(nodes.filterNot(_.equals(node))))).foreach{shard =>
+      sortedMap.put(hash(shard), shard)
+    }
+    this
+  }
+
+  def findCandidate(hash: Integer) = {
+    if (sortedMap.containsKey(hash)) hash -> sortedMap.get(hash)
+    else {
+      val tailMap = sortedMap.tailMap(hash)
+      val newHash = if (tailMap.isEmpty) sortedMap.firstKey() else tailMap.firstKey()
+      newHash -> sortedMap.get(newHash)
+    }
+  }
+
+  def find(key: Array[Byte]): Option[Shard] = {
+    if (sortedMap.isEmpty) return None
+
+    val hashIdx = hashFn.hash(key)
+    val (newHash, candidate) = findCandidate(hashIdx)
+
+    candidate.getNodes
+  }
+
+}
