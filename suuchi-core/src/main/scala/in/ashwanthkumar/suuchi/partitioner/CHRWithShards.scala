282dc1dbc2e58727eb8059caf595f1d1457c6c4b
Selvaram Ganesh <gsriram7@gmail.com>
2016-12-01 19:18:40 +0530

    Using a priority queue approach to assign followers to shards
    Added tests for NodePriority
    
    P.S More tests need to be added to CHRWithShards and method and class names of NodePriority has to be refactored

--- suuchi-core/src/main/scala/in/ashwanthkumar/suuchi/partitioner/CHRWithShards.scala
+++ suuchi-core/src/main/scala/in/ashwanthkumar/suuchi/partitioner/CHRWithShards.scala
@@ -4,29 +4,30 @@ import java.util
 
 import in.ashwanthkumar.suuchi.cluster.MemberAddress
 
-case class Shard(primaryPartition: VNode, follower: List[VNode]) {
-  def key = primaryPartition.node.host + "_" + primaryPartition.node.port + "_" + primaryPartition.nodeReplicaId
+case class Shard(id: Int, primaryPartition: VNode, followers: List[VNode]) {
+  def key = id + "_" + primaryPartition.node.host + "_" + primaryPartition.node.port
 
-  def getNodes = primaryPartition.node :: follower.map(_.node)
+  def getNodes = primaryPartition.node :: followers.map(_.node)
 }
 
-class CHRWithShards(hashFn: Hash, partitionsPerNode: Int, replicationFactor: Int = 2) {
-  val sortedMap = new util.TreeMap[Integer, Shard]()
+class CHRWithShards(hashFn: Hash, primaryPartitionsPerNode: Int, replicationFactor: Int = 2) {
+  private val sortedMap = new util.TreeMap[Integer, Shard]()
+  private val priority = new NodePriority
 
   def init(nodes: List[MemberAddress]) = {
-    nodes.foreach(node => add(node, nodes))
+    priority.init(nodes)
+    nodes.foreach(node => add(node))
     this
   }
 
   private def hash(shard: Shard): Int = hashFn.hash(shard.key.getBytes)
 
-  def followers(nodes: List[MemberAddress]): List[VNode] = {
-    nodes.flatMap(node => (1 until replicationFactor).map(i => VNode(node, i, Follower)))
+  def followers(node: MemberAddress): List[VNode] = {
+    priority.followers(replicationFactor - 1, node).map(address => VNode(address, 1, Follower))
   }
 
-  def add(node: MemberAddress, nodes: List[MemberAddress]) = {
-    // FIXME: followers based on nodes.filterNot will lead to a skew in first elements in the list
-    (1 to partitionsPerNode).map(i => Shard(VNode(node, 1, Primary), followers(nodes.filterNot(_.equals(node))))).foreach { shard =>
+  private def add(node: MemberAddress) = {
+    (1 to primaryPartitionsPerNode).map(i => Shard(i, VNode(node, 1, Primary), followers(node))).foreach { shard =>
       sortedMap.put(hash(shard), shard)
     }
     this
@@ -49,4 +50,12 @@ class CHRWithShards(hashFn: Hash, partitionsPerNode: Int, replicationFactor: Int
     candidates.getNodes
   }
 
+  // USED ONLY FOR TESTS
+  private[partitioner] def shards = sortedMap.values()
+
+}
+
+object CHRWithShards {
+  def apply(hashFn: Hash, nodes: List[MemberAddress], primaryPartitionsPerNode: Int, replicationFactor: Int): CHRWithShards = new CHRWithShards(hashFn, primaryPartitionsPerNode, replicationFactor).init(nodes)
+  def apply(nodes: List[MemberAddress], primaryPartitionsPerNode: Int, replicationFactor: Int): CHRWithShards = apply(SuuchiHash, nodes, primaryPartitionsPerNode, replicationFactor)
 }
