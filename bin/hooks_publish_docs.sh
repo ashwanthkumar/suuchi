4575e8b3449d2852d555439a71b9037f2a019a54
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2017-05-26 10:55:21 +0530

    Fixing the doc publish hook

--- bin/hooks_publish_docs.sh
+++ bin/hooks_publish_docs.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-- git config user.name "Ashwanth Kumar"
-- git config user.email "ashwanthkumar@googlemail.com"
-- git remote add gh-token "https://${GH_TOKEN}@github.com/ashwanthkumar/suuchi.git";
-- git fetch gh-token && git fetch gh-token gh-pages:gh-pages;
-- sudo pip install mkdocs==0.15.3
-- mkdocs gh-deploy -v --clean --remote-name gh-token;
+
+git config user.name "Ashwanth Kumar"
+git config user.email "ashwanthkumar@googlemail.com"
+git remote add gh-token "https://${GH_TOKEN}@github.com/ashwanthkumar/suuchi.git";
+git fetch gh-token && git fetch gh-token gh-pages:gh-pages;
+sudo pip install mkdocs==0.15.3
+mkdocs gh-deploy -v --clean --remote-name gh-token;
