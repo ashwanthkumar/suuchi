ce04ed6e828c110b049b81022306b8ba2cb32d31
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2017-06-02 23:13:55 +0530

    Even run the git commands for the docs only when on master

--- bin/hooks_publish_docs.sh
+++ bin/hooks_publish_docs.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-git config user.name "Ashwanth Kumar"
-git config user.email "ashwanthkumar@googlemail.com"
-git remote add gh-token "https://${GH_TOKEN}@github.com/ashwanthkumar/suuchi.git";
-git fetch gh-token && git fetch gh-token gh-pages:gh-pages;
-
 # Deploy Docs only for builds out of master and not PRs or tags.
 if ([ "$TRAVIS_BRANCH" == "master" ] || [ ! -z "$TRAVIS_TAG" ]) &&
   [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    git config user.name "Ashwanth Kumar"
+    git config user.email "ashwanthkumar@googlemail.com"
+    git remote add gh-token "https://${GH_TOKEN}@github.com/ashwanthkumar/suuchi.git";
+    git fetch gh-token && git fetch gh-token gh-pages:gh-pages;
+
     virtualenv --system-site-packages ${HOME}/DENV
     source ${HOME}/DENV/bin/activate
     pip install mkdocs==0.15.3
