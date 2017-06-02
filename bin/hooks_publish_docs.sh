be981871b4fbc2485aac49512d52f4fa423661d4
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2017-06-02 15:02:26 +0530

    Doing mkdocs only for master branch

--- bin/hooks_publish_docs.sh
+++ bin/hooks_publish_docs.sh
@@ -4,5 +4,10 @@ git config user.name "Ashwanth Kumar"
 git config user.email "ashwanthkumar@googlemail.com"
 git remote add gh-token "https://${GH_TOKEN}@github.com/ashwanthkumar/suuchi.git";
 git fetch gh-token && git fetch gh-token gh-pages:gh-pages;
-sudo pip install mkdocs==0.15.3
-mkdocs gh-deploy -v --clean --remote-name gh-token;
+
+# Deploy Docs only for builds out of master and not PRs or tags.
+if ([ "$TRAVIS_BRANCH" == "master" ] || [ ! -z "$TRAVIS_TAG" ]) &&
+  [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    sudo pip install mkdocs==0.15.3
+    mkdocs gh-deploy -v --clean --remote-name gh-token;
+fi
