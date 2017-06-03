f198a9e09c4acc94ce941c6a90aa90c20fc415ee
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2017-06-03 07:35:58 +0530

    Adding log statements in .travis/release.sh

--- .travis/release.sh
+++ .travis/release.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-if [ "${TRAVIS_COMMIT_MESSAGE}" == "[Do Release]" ];
+if [ "$TRAVIS_COMMIT_MESSAGE" == "[Do Release]" ];
 then
+    echo "Triggering a versioned release of the project"
     # Git configurations while doing the version upgrade and commit
     git config user.name "Ashwanth Kumar"
     git config user.email "ashwanthkumar@googlemail.com"
     git remote remove origin
     git remote add origin "https://${GH_TOKEN}@github.com/ashwanthkumar/suuchi.git";
-
-    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=${TRAVIS_TAG} 1>/dev/null 2>/dev/null
 
     if [ ! -z "$TRAVIS" -a -f "$HOME/.gnupg" ]; then
         shred -v ~/.gnupg/*
@@ -17,6 +16,9 @@ then
     source .travis/gpg.sh
 
     mvn release:perform --settings .travis/settings.xml -DskipTests=true -DperformRelease --batch-mode --update-snapshots
+    echo "Versioned release of the project is now complete"
 else
+    echo "Triggering a SNAPSHOT release of the project"
     mvn deploy --settings .travis/settings.xml -DskipTests=true -B
+    echo "SNAPSHOT release of the project is now complete"
 fi
