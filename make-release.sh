4c5ee26b77212cd2ac6303f50aa219d3c584488f
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2017-06-03 12:08:14 +0530

    Doing stash --all instead of just git stash

--- make-release.sh
+++ make-release.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git stash
+git stash --all
 # We create a special type of commit message to trigger a release workflow in travis-ci
 git commit --allow-empty -m "[Do Release]"
 git push
