d1fe5da0557ba6f81e162b898ad96ab5bb13e748
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2019-09-26 11:48:48 +0530

    Install requirements from the file

--- .travis/docs.sh
+++ .travis/docs.sh
@@ -10,6 +10,6 @@ if ([ "$TRAVIS_BRANCH" == "master" ] || [ ! -z "$TRAVIS_TAG" ]) &&
 
     virtualenv --system-site-packages ${HOME}/DENV
     source ${HOME}/DENV/bin/activate
-    pip install mkdocs==0.15.3
+    pip install -r docs-requirements.txt
     mkdocs gh-deploy -v --clean --remote-name gh-token;
 fi
